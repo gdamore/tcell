gdamore/tcell

commit d400d66308e21813c9cb19cbcfc075eee0b3ee3b

Defer screen finish in app run

views/app.go
@@ -121,6 +121,7 @@ func (app *Application) run() {
 		}
 		screen = app.screen
 	}
+	defer screen.Fini()
 	screen.Init()
 	screen.Clear()
 	widget.SetView(screen)
@@ -150,7 +151,6 @@ loop:
 			widget.HandleEvent(ev)
 		}
 	}
-	screen.Fini()
 	app.wg.Done()
 }
 

commit 82b7404080bf9223889a241b2bbc3344b3fee179

fix regression caused by #177 in cleaning up
The defer of the screen.Fini() needed to include the defer of the
app.wg.Done(), and the original wg.Done needed to be eliminated.
The problem was originally spotted by @thechriswalker, with a proposed
fix that was close but not quite correct.

views/app.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Tcell Authors
+// Copyright 2018 The Tcell Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use file except in compliance with the License.
@@ -121,7 +121,10 @@ func (app *Application) run() {
 		}
 		screen = app.screen
 	}
-	defer screen.Fini()
+	defer func() {
+		screen.Fini()
+		app.wg.Done()
+	}()
 	screen.Init()
 	screen.Clear()
 	widget.SetView(screen)
@@ -151,7 +154,6 @@ loop:
 			widget.HandleEvent(ev)
 		}
 	}
-	app.wg.Done()
 }
 
 // Start starts the application loop, initializing the screen