gdamore/tcell

commit 2d6d7fbe48de9e009b12c284c804b130eb4fa009

Change interpretation of pushing quoted character in terminfo parser
I noticed this problem while running a gowid test program (palette.go)
with TERM=xterm-16color. This terminal type is not present in tcell's
built-in database, and so tcell falls back to the dynamic terminal type
by parsing the output of infocmp. The symptom was that foreground colors
were not correctly set, leaving a monochrome screen. This seems to be
caused by a problem interpreting the *background* color terminfo
rule. The attribute to set background color is defined like this (Ubuntu
20.04):

$ infocmp xterm-16color | grep setab
	setab=\E[%?%p1%{8}%<%t%p1%'('%+%e%p1%{92}%+%;%dm,

The middle sections says "if p1<8 then push(p1+'(') ". '(' is ascii
40. If I run

$ tput setab 5

the terminal sees 'ESC[45m'. This correctly sets the background color to
magenta. But if I tell tcell to emit a cell with background color set to
tcell.Color(5), the terminal sees 'ESC[0m'. This means in practice, my
app emits a code to set the foreground color, then an SGR code that
resets all attributes, then the ASCII character.

When tcell "runs" a terminfo rule in terminfo::TParm(), a push to the
stack preserves the type of the argument pushed - int or string. When a
single quote is encountered, the argument within is pushed to the stack
as a string. For the `setab` rule above, tcell will then pop as an int,
discarding the error and returning 0. The fix here is to have tcell push
the argument inside the single quotes as an integer, using the ascii
value of the argument e.g. "(" -> 40 - and assume the string is length
1, I suppose. Cross-referencing against
ncurses/tinfo/lib_tinfo.c::tparam_internal(), it looks like this code
assumes a single-quoted string is assumed to be length=1 and is also
interpreted as an integer with the ascii value of the character:

case S_QUOTE:
    cp++;
    npush(UChar(*cp));
    cp++;
    break;

terminfo/terminfo.go
@@ -461,7 +461,7 @@ func (t *Terminfo) TParm(s string, p ...int) string {
 		case '\'': // push(char)
 			ch, _ = pb.NextCh()
 			pb.NextCh() // must be ' but we don't check
-			stk = stk.Push(string(ch))
+			stk = stk.PushInt(int(ch))
 
 		case '{': // push(int)
 			ai = 0

commit e861cbfa427f7ad293a7414c730a517d8c439f48

Attempted fix of a regression I introduced - breaking mouse support!
@tslocum reported that the commit at 2d6d7fbe broke mouse support in
cview (https://github.com/gdamore/tcell/pull/384#issuecomment-698422464). The
purpose of the original commit was to fix a terminfo background color
code parsing problem that I spotted using TERM=xterm-16color:

$ infocmp xterm-16color | grep setab
	setab=\E[%?%p1%{8}%<%t%p1%'('%+%e%p1%{92}%+%;%dm,

The middle sections says "if p1<8 then push(p1+'(') ". '(' is ascii
40. The sequence '%d' will then pop from the stack and interpreting the
result as an integer.

My change above was to have tcell assume the sequence between two single
quotes was a single ASCII character e.g. '(' and push that to the
terminfo stack as an integer with the ASCII value of the character
e.g. push(40:int). I didn't find any counterexamples in my local
terminfo database that didn't line up with that assumption; but I did
break mouse support because tcell generates its own rules to enable
mouse support, if not specified in the terminfo DB - both when
generating its own Golang-style internal DB, and in the dynamic terminfo
generator that uses infocmp:

"%?%p1%{1}%=%t%'h'%Pa%e%'l'%Pa%;\x1b[?1000%ga%c\x1b[?1002%ga%c\x1b[?1003%ga%c\x1b[?1006%ga%c"

This rule will push 'h' or 'l', depending on whether mouse mode is to be
enabled or disabled, and then sets the variable named a to the result,
interpreted as a string. The result is that when mouse mode is enabled,
tcell needs to emit \x1b[?1000h - where the 'h' comes from variable a
i.e. it needs to be able to push a quoted argument as a string and not
be forced into an int.

This alternative fix preserves the fact the quoted argument is a string
and maintains that on the stack. However, if the integer Pop() function
is called when the top element of the stack is a string, tcell will now

- return the string converted to an int, if it converts e.g. in the case

terminfo/terminfo.go
@@ -260,8 +260,17 @@ func (st stack) PopInt() (int, stack) {
 		if e.isInt {
 			return e.i, st
 		} else if e.isStr {
-			i, _ := strconv.Atoi(e.s)
-			return i, st
+			// If the string that was pushed was the representation
+			// of a number e.g. '123', then return the number. If the
+			// conversion doesn't work, assume the string pushed was
+			// intended to return, as an int, the ascii representation
+			// of the (one and only) character.
+			i, err := strconv.Atoi(e.s)
+			if err == nil {
+				return i, st
+			} else if len(e.s) >= 1 {
+				return int(e.s[0]), st
+			}
 		}
 	}
 	return 0, st
@@ -461,7 +470,7 @@ func (t *Terminfo) TParm(s string, p ...int) string {
 		case '\'': // push(char)
 			ch, _ = pb.NextCh()
 			pb.NextCh() // must be ' but we don't check
-			stk = stk.PushInt(int(ch))
+			stk = stk.Push(string(ch))
 
 		case '{': // push(int)
 			ai = 0