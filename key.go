gdamore/tcell

commit 74fd4877e82c639eba6ca0b7c9c94b6a39c3d837

feat: Modern XTerm CSI-u key reporting (fixes #671)
This now discriminates between e.g. Tab and Ctrl-I.  But it does require
support from the terminal emulator.  Many do.

Modifiers have changed (we added ModHyper) and some keys
have been added as well.  More will be added later.
With this improvement we actually have the ability to obtain modifiers
for some keys that we otherwise would not.

Windows key reporting is somewhat harmonized as well, so that we get as
close to the same experience on all platforms as possible.

key.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The TCell Authors
+// Copyright 2025 The TCell Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use file except in compliance with the License.
@@ -171,6 +171,11 @@ var KeyNames = map[Key]string{
 	KeyF62:            "F62",
 	KeyF63:            "F63",
 	KeyF64:            "F64",
+	KeyMenu:           "Menu",
+	KeyCapsLock:       "CapsLock",
+	KeyScrollLock:     "ScrollLock",
+	KeyNumLock:        "NumLock",
+	KeyCtrlSpace:      "Ctrl-Space",
 	KeyCtrlA:          "Ctrl-A",
 	KeyCtrlB:          "Ctrl-B",
 	KeyCtrlC:          "Ctrl-C",
@@ -178,9 +183,12 @@ var KeyNames = map[Key]string{
 	KeyCtrlE:          "Ctrl-E",
 	KeyCtrlF:          "Ctrl-F",
 	KeyCtrlG:          "Ctrl-G",
+	KeyCtrlH:          "Ctrl-H",
+	KeyCtrlI:          "Ctrl-I",
 	KeyCtrlJ:          "Ctrl-J",
 	KeyCtrlK:          "Ctrl-K",
 	KeyCtrlL:          "Ctrl-L",
+	KeyCtrlM:          "Ctrl-M",
 	KeyCtrlN:          "Ctrl-N",
 	KeyCtrlO:          "Ctrl-O",
 	KeyCtrlP:          "Ctrl-P",
@@ -194,11 +202,11 @@ var KeyNames = map[Key]string{
 	KeyCtrlX:          "Ctrl-X",
 	KeyCtrlY:          "Ctrl-Y",
 	KeyCtrlZ:          "Ctrl-Z",
-	KeyCtrlSpace:      "Ctrl-Space",
-	KeyCtrlUnderscore: "Ctrl-_",
-	KeyCtrlRightSq:    "Ctrl-]",
+	KeyCtrlLeftSq:     "Ctrl-[",
+	KeyCtrlRightSq:    "Ctrl-[",
 	KeyCtrlBackslash:  "Ctrl-\\",
 	KeyCtrlCarat:      "Ctrl-^",
+	KeyCtrlUnderscore: "Ctrl-_",
 }
 
 // Name returns a printable value or the key stroke.  This can be used
@@ -217,6 +225,9 @@ func (ev *EventKey) Name() string {
 	}
 	if ev.mod&ModCtrl != 0 {
 		m = append(m, "Ctrl")
+	}
+	if ev.mod&ModHyper != 0 {
+		m = append(m, "Hyper")
 	}
 
 	ok := false
@@ -252,9 +263,27 @@ func NewEventKey(k Key, ch rune, mod ModMask) *EventKey {
 			default:
 				// most likely entered with a CTRL keypress
 				mod = ModCtrl
+				ch = ch + '\x60'
 			}
 		}
 	}
+	if k == KeyRune && ch >= '@' && ch <= '_' && mod == ModCtrl {
+		// We don't do Ctrl-[ or backslash or those specially.
+		k = KeyCtrlA + Key(ch-'@')
+	}
+
+	// Might be lower case
+	if k == KeyRune && ch >= 'a' && ch <= 'z' && mod == ModCtrl {
+		// We don't do Ctrl-[ or backslash or those specially.
+		k = KeyCtrlA + Key(ch-'a')
+	}
+
+	// Windows reports ModShift for shifted keys.  This is inconsistent
+	// with UNIX, lets harmonize this.
+	if k == KeyRune && mod == ModShift && ch != 0 {
+		mod = ModNone
+	}
+
 	return &EventKey{t: time.Now(), key: k, ch: ch, mod: mod}
 }
 
@@ -272,6 +301,7 @@ const (
 	ModCtrl
 	ModAlt
 	ModMeta
+	ModHyper
 	ModNone ModMask = 0
 )
 
@@ -373,6 +403,10 @@ const (
 	KeyF62
 	KeyF63
 	KeyF64
+	KeyMenu
+	KeyCapsLock
+	KeyScrollLock
+	KeyNumLock
 )
 
 const (
@@ -383,8 +417,11 @@ const (
 
 // These are the control keys.  Note that they overlap with other keys,
 // perhaps.  For example, KeyCtrlH is the same as KeyBackspace.
+//
+// Deprecated: Most of these now are reported using KeyRune and modifiers,
+// or other direct mappings.
 const (
-	KeyCtrlSpace Key = iota
+	KeyCtrlSpace Key = iota + 64
 	KeyCtrlA
 	KeyCtrlB
 	KeyCtrlC

console_win.go
@@ -790,6 +790,10 @@ func (s *cScreen) getConsoleInput() error {
 			if krec.ch != 0 {
 				// synthesized key code
 				for krec.repeat > 0 {
+					if krec.ch < ' ' && mod2mask(krec.mod, false) == ModCtrl {
+						krec.ch += '\x60'
+					}
+
 					// convert shift+tab to backtab
 					if mod2mask(krec.mod, false) == ModShift && krec.ch == vkTab {
 						s.postEvent(NewEventKey(KeyBacktab, 0, ModNone))

simulation.go
@@ -356,11 +356,18 @@ outer:
 		}
 
 		if b[0] < 0x80 {
-			mod := ModNone
 			// No encodings start with low numbered values
-			if Key(b[0]) >= KeyCtrlA && Key(b[0]) <= KeyCtrlZ {
-				mod = ModCtrl
+			if b[0] > 0 && b[0] < ' ' { // control keys
+				switch Key(b[0]) {
+				case KeyESC, KeyEnter, KeyTAB:
+					s.postEvent(NewEventKey(Key(b[0]), 0, 0))
+					continue;
+				default:
+					s.postEvent(NewEventKey(Key(b[0]), rune(b[0])+'\x60', ModCtrl))
+					continue
+				}
 			}
+			mod := ModNone
 			ev := NewEventKey(Key(b[0]), 0, mod)
 			s.postEvent(ev)
 			b = b[1:]

termbox/compat.go
@@ -234,26 +234,27 @@ const (
 
 // Keys codes.
 const (
-	KeyF1             = Key(tcell.KeyF1)
-	KeyF2             = Key(tcell.KeyF2)
-	KeyF3             = Key(tcell.KeyF3)
-	KeyF4             = Key(tcell.KeyF4)
-	KeyF5             = Key(tcell.KeyF5)
-	KeyF6             = Key(tcell.KeyF6)
-	KeyF7             = Key(tcell.KeyF7)
-	KeyF8             = Key(tcell.KeyF8)
-	KeyF9             = Key(tcell.KeyF9)
-	KeyF10            = Key(tcell.KeyF10)
-	KeyF11            = Key(tcell.KeyF11)
-	KeyF12            = Key(tcell.KeyF12)
-	KeyInsert         = Key(tcell.KeyInsert)
-	KeyDelete         = Key(tcell.KeyDelete)
-	KeyHome           = Key(tcell.KeyHome)
-	KeyEnd            = Key(tcell.KeyEnd)
-	KeyArrowUp        = Key(tcell.KeyUp)
-	KeyArrowDown      = Key(tcell.KeyDown)
-	KeyArrowRight     = Key(tcell.KeyRight)
-	KeyArrowLeft      = Key(tcell.KeyLeft)
+	KeyF1         = Key(tcell.KeyF1)
+	KeyF2         = Key(tcell.KeyF2)
+	KeyF3         = Key(tcell.KeyF3)
+	KeyF4         = Key(tcell.KeyF4)
+	KeyF5         = Key(tcell.KeyF5)
+	KeyF6         = Key(tcell.KeyF6)
+	KeyF7         = Key(tcell.KeyF7)
+	KeyF8         = Key(tcell.KeyF8)
+	KeyF9         = Key(tcell.KeyF9)
+	KeyF10        = Key(tcell.KeyF10)
+	KeyF11        = Key(tcell.KeyF11)
+	KeyF12        = Key(tcell.KeyF12)
+	KeyInsert     = Key(tcell.KeyInsert)
+	KeyDelete     = Key(tcell.KeyDelete)
+	KeyHome       = Key(tcell.KeyHome)
+	KeyEnd        = Key(tcell.KeyEnd)
+	KeyArrowUp    = Key(tcell.KeyUp)
+	KeyArrowDown  = Key(tcell.KeyDown)
+	KeyArrowRight = Key(tcell.KeyRight)
+	KeyArrowLeft  = Key(tcell.KeyLeft)
+	// Provided for binary compatibility, but may not be usable.
 	KeyCtrlA          = Key(tcell.KeyCtrlA)
 	KeyCtrlB          = Key(tcell.KeyCtrlB)
 	KeyCtrlC          = Key(tcell.KeyCtrlC)

tscreen.go
@@ -1,4 +1,4 @@
-// Copyright 2024 The TCell Authors
+// Copyright 2025 The TCell Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use file except in compliance with the License.
@@ -178,6 +178,8 @@ type tScreen struct {
 	setClipboard string
 	startSyncOut string
 	endSyncOut   string
+	enableCsiU   string
+	disableCsiU  string
 
 	sync.Mutex
 }
@@ -464,6 +466,11 @@ func (t *tScreen) prepareExtendedOSC() {
 		t.startSyncOut = "\x1b[?2026h"
 		t.endSyncOut = "\x1b[?2026l"
 	}
+
+	if t.enableCsiU == "" && t.ti.XTermLike {
+		t.enableCsiU = "\x1b[>1u"
+		t.disableCsiU = "\x1b[<u"
+	}
 }
 
 func (t *tScreen) prepareCursorStyles() {
@@ -670,15 +677,14 @@ outer:
 			}
 		}
 
-		t.keyexist[Key(i)] = true
-
 		mod := ModCtrl
 		switch Key(i) {
 		case KeyBS, KeyTAB, KeyESC, KeyCR:
 			// directly type-able- no control sequence
 			mod = ModNone
+			t.keycodes[string(rune(i))] = &tKeyCode{key: Key(i), mod: mod}
+			t.keyexist[Key(i)] = true
 		}
-		t.keycodes[string(rune(i))] = &tKeyCode{key: Key(i), mod: mod}
 	}
 }
 
@@ -1678,6 +1684,132 @@ func (t *tScreen) parseFunctionKey(buf *bytes.Buffer, evs *[]Event) (bool, bool)
 	return partial, false
 }
 
+var keysCsiU = map[rune]Key{
+	27:    KeyESC,
+	9:     KeyTAB,
+	13:    KeyEnter,
+	127:   KeyBS,
+	57358: KeyNUL, // KeyCapsLock
+	57359: KeyNUL, // KeyScrollLock
+	57360: KeyNUL, // KeyNumLock
+	57361: KeyPrint,
+	57362: KeyPause,
+	57363: KeyNUL, // KeyMenu
+	57376: KeyF13,
+	57377: KeyF14,
+	57378: KeyF15,
+	57379: KeyF16,
+	57380: KeyF17,
+	57381: KeyF18,
+	57382: KeyF19,
+	57383: KeyF20,
+	57384: KeyF21,
+	57385: KeyF22,
+	57386: KeyF23,
+	57387: KeyF24,
+	57388: KeyF25,
+	57389: KeyF26,
+	57390: KeyF27,
+	57391: KeyF28,
+	57392: KeyF29,
+	57393: KeyF30,
+	57394: KeyF31,
+	57395: KeyF32,
+	57396: KeyF33,
+	57397: KeyF34,
+	57398: KeyF35,
+	// TODO: KP keys
+	// TODO: Media keys
+}
+
+func (t *tScreen) parseCsiU(buf *bytes.Buffer, evs *[]Event) (bool, bool) {
+	b := buf.Bytes()
+
+	switch len(b) {
+	case 0:
+		return false, false
+	case 1:
+		return b[0] == '\x1b', false
+	case 2:
+		return b[0] == '\x1b' && b[1] == '[', false // CSI introducer
+	}
+	if b[0] != '\x1b' || b[1] != '[' {
+		return false, false
+	}
+	seq := ""
+	for i := 2; i < len(b); i++ {
+		if b[i] >= 0x20 && b[i] < 0x3F {
+			continue
+		}
+		if b[i] == 'u' {
+			seq = string(b[2:i]) // just the "inner" parts (without CSI or the u)
+			for range i + 1 {    // +1 because zero based
+				_, _ = buf.ReadByte()
+			}
+			break
+		}
+		return false, false
+	}
+
+	// matched a CSI-u
+	parts := strings.Split(seq, ";")
+	var k Key
+	var ch rune
+	var mod ModMask
+	if num, err := strconv.ParseInt(parts[0], 10, 32); err != nil {
+		return false, false
+	} else {
+		ch = rune(num)
+		k = KeyRune
+		if k1, ok := keysCsiU[ch]; ok {
+			k = k1
+			ch = 0
+		}
+	}
+
+	if len(parts) > 1 {
+		// shift     0b1         (1)
+		// alt       0b10        (2)
+		// ctrl      0b100       (4)
+		// super     0b1000      (8)
+		// hyper     0b10000     (16)
+		// meta      0b100000    (32)
+		// caps_lock 0b1000000   (64)
+		// num_lock  0b10000000  (128)
+
+		if mm, err := strconv.Atoi(parts[1]); err != nil {
+			return false, false
+		} else {
+			mm -= 1
+			if mm&0x1 != 0 {
+				mod |= ModShift
+			}
+			if mm&0x2 != 0 {
+				mod |= ModAlt
+			}
+			if mm&0x4 != 0 {
+				mod |= ModCtrl
+			}
+			// NB: Kitty calls this Super, but, we mostly call this Meta everywhere.
+			if mm&0x8 != 0 {
+				mod |= ModMeta
+			}
+			if mm&0x10 != 0 {
+				mod |= ModHyper
+			}
+			if mm&0x20 != 0 {
+				// In theory we might want to disambiguate this, but we don't
+				// want to introduce Super, and it probably doesn't exist in
+				// practice.
+				mod |= ModMeta
+			}
+			// TODO : super = 0x8, hyper = 0x10, caps_lock = 0x40, num_lock = 0x80
+		}
+	}
+	*evs = append(*evs, NewEventKey(k, ch, mod))
+	return false, true
+}
+
 func (t *tScreen) parseRune(buf *bytes.Buffer, evs *[]Event) (bool, bool) {
 	b := buf.Bytes()
 	if b[0] >= ' ' && b[0] <= 0x7F {
@@ -1799,6 +1931,14 @@ func (t *tScreen) collectEventsFromInput(buf *bytes.Buffer, expire bool) []Event
 			}
 		}
 
+		if t.enableCsiU != "" {
+			if part, comp := t.parseCsiU(buf, &res); comp {
+				continue
+			} else if part {
+				partials++
+			}
+		}
+
 		if partials == 0 || expire {
 			if b[0] == '\x1b' {
 				if len(b) == 1 {
@@ -1819,6 +1959,15 @@ func (t *tScreen) collectEventsFromInput(buf *bytes.Buffer, expire bool) []Event
 			if t.escaped {
 				t.escaped = false
 				mod = ModAlt
+			}
+			if by < ' ' {
+				switch by {
+				case byte(KeyTAB), byte(KeyEnter), byte(KeyESC), byte(KeyBS):
+					break
+				default:
+					by += '\x60'
+					mod |= ModCtrl
+				}
 			}
 			res = append(res, NewEventKey(KeyRune, rune(by), mod))
 			continue
@@ -2040,7 +2189,6 @@ func (t *tScreen) engage() error {
 	if t.focusEnabled {
 		t.enableFocusReporting()
 	}
-
 	ti := t.ti
 	if os.Getenv("TCELL_ALTSCREEN") != "disable" {
 		// Technically this may not be right, but every terminal we know about
@@ -2049,9 +2197,7 @@ func (t *tScreen) engage() error {
 		// (In theory there could be terminals that don't support X,Y cursor
 		// positions without a setup command, but we don't support them.)
 		t.TPuts(ti.EnterCA)
-		if t.saveTitle != "" {
-			t.TPuts(t.saveTitle)
-		}
+		t.TPuts(t.saveTitle)
 	}
 	t.TPuts(ti.EnterKeypad)
 	t.TPuts(ti.HideCursor)
@@ -2061,6 +2207,7 @@ func (t *tScreen) engage() error {
 	if t.title != "" && t.setTitle != "" {
 		t.TPuts(t.ti.TParm(t.setTitle, t.title))
 	}
+	t.TPuts(t.enableCsiU)
 
 	t.wg.Add(2)
 	go t.inputLoop(stopQ)
@@ -2079,6 +2226,7 @@ func (t *tScreen) disengage() {
 		t.Unlock()
 		return
 	}
+
 	t.running = false
 	stopQ := t.stopQ
 	close(stopQ)
@@ -2113,6 +2261,7 @@ func (t *tScreen) disengage() {
 	t.enableMouse(0)
 	t.enablePasting(false)
 	t.disableFocusReporting()
+	t.TPuts(t.disableCsiU)
 
 	_ = t.tty.Stop()
 }

wscreen.go
@@ -20,7 +20,6 @@ package tcell
 import (
 	"errors"
 	"fmt"
-	"strings"
 	"sync"
 	"syscall/js"
 	"unicode/utf8"
@@ -382,14 +381,6 @@ func (t *wScreen) onKeyEvent(this js.Value, args []js.Value) interface{} {
 		mod |= ModMeta
 	}
 
-	// check for special case of Ctrl + key
-	if mod == ModCtrl {
-		if k, ok := WebKeyNames["Ctrl-"+strings.ToLower(key)]; ok {
-			t.postEvent(NewEventKey(k, 0, mod))
-			return nil
-		}
-	}
-
 	// next try function keys
 	if k, ok := WebKeyNames[key]; ok {
 		t.postEvent(NewEventKey(k, 0, mod))
@@ -631,34 +622,6 @@ var WebKeyNames = map[string]Key{
 	"F62":        KeyF62,
 	"F63":        KeyF63,
 	"F64":        KeyF64,
-	"Ctrl-a":     KeyCtrlA,          // not reported by HTML- need to do special check
-	"Ctrl-b":     KeyCtrlB,          // not reported by HTML- need to do special check
-	"Ctrl-c":     KeyCtrlC,          // not reported by HTML- need to do special check
-	"Ctrl-d":     KeyCtrlD,          // not reported by HTML- need to do special check
-	"Ctrl-e":     KeyCtrlE,          // not reported by HTML- need to do special check
-	"Ctrl-f":     KeyCtrlF,          // not reported by HTML- need to do special check
-	"Ctrl-g":     KeyCtrlG,          // not reported by HTML- need to do special check
-	"Ctrl-j":     KeyCtrlJ,          // not reported by HTML- need to do special check
-	"Ctrl-k":     KeyCtrlK,          // not reported by HTML- need to do special check
-	"Ctrl-l":     KeyCtrlL,          // not reported by HTML- need to do special check
-	"Ctrl-n":     KeyCtrlN,          // not reported by HTML- need to do special check
-	"Ctrl-o":     KeyCtrlO,          // not reported by HTML- need to do special check
-	"Ctrl-p":     KeyCtrlP,          // not reported by HTML- need to do special check
-	"Ctrl-q":     KeyCtrlQ,          // not reported by HTML- need to do special check
-	"Ctrl-r":     KeyCtrlR,          // not reported by HTML- need to do special check
-	"Ctrl-s":     KeyCtrlS,          // not reported by HTML- need to do special check
-	"Ctrl-t":     KeyCtrlT,          // not reported by HTML- need to do special check
-	"Ctrl-u":     KeyCtrlU,          // not reported by HTML- need to do special check
-	"Ctrl-v":     KeyCtrlV,          // not reported by HTML- need to do special check
-	"Ctrl-w":     KeyCtrlW,          // not reported by HTML- need to do special check
-	"Ctrl-x":     KeyCtrlX,          // not reported by HTML- need to do special check
-	"Ctrl-y":     KeyCtrlY,          // not reported by HTML- need to do special check
-	"Ctrl-z":     KeyCtrlZ,          // not reported by HTML- need to do special check
-	"Ctrl- ":     KeyCtrlSpace,      // not reported by HTML- need to do special check
-	"Ctrl-_":     KeyCtrlUnderscore, // not reported by HTML- need to do special check
-	"Ctrl-]":     KeyCtrlRightSq,    // not reported by HTML- need to do special check
-	"Ctrl-\\":    KeyCtrlBackslash,  // not reported by HTML- need to do special check
-	"Ctrl-^":     KeyCtrlCarat,      // not reported by HTML- need to do special check
 }
 
 var curStyleClasses = map[CursorStyle]string{

commit 22617742b7764275261f79195a722b226d77e444

Fix KeyCtrlRightSq name from 'Ctrl-[' to 'Ctrl-]'
KeyCtrlRightSq (Ctrl+]) was incorrectly named 'Ctrl-[' instead of 'Ctrl-]'.
This appears to be a copy-paste error introduced in commit 74fd487.

This fix corrects the KeyNames map entry for KeyCtrlRightSq to properly
reflect the right square bracket ']' instead of the left square bracket '['.

key.go
@@ -203,7 +203,7 @@ var KeyNames = map[Key]string{
 	KeyCtrlY:          "Ctrl-Y",
 	KeyCtrlZ:          "Ctrl-Z",
 	KeyCtrlLeftSq:     "Ctrl-[",
-	KeyCtrlRightSq:    "Ctrl-[",
+	KeyCtrlRightSq:    "Ctrl-]",
 	KeyCtrlBackslash:  "Ctrl-\\",
 	KeyCtrlCarat:      "Ctrl-^",
 	KeyCtrlUnderscore: "Ctrl-_",