gdamore/tcell

commit 5c18b2ff29ccbc80703ef93ea78af6e2e6c6caf5

chore: refactor color support to use ECMA SGR universally
This relies on COLORTERM to detect 24-bit color support for now (or
a terminal name ending with -direct or -truecolor), and uses the
same ANSI escapes everywhere.  This removes this from the terminfo
descriptions and is another step on the teardown of terminfo.

tscreen.go
@@ -21,6 +21,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"io"
 	"maps"
 	"os"
@@ -91,9 +92,18 @@ const (
 	underColor        = "\x1b[58:5:%p1%dm"
 	underRGB          = "\x1b[58:2::%p1%d:%p2%d:%p3%dm"
 	underFg           = "\x1b[59m"
-	enableAltChars    = "\x1b(B\x1b)0" // set G0 as US-ASCII, G1 as DEC line drawing
-	startAltChars     = "\x0e"         // aka Shift-Out
-	endAltChars       = "\x0f"         // aka Shift-In
+	enableAltChars    = "\x1b(B\x1b)0"                       // set G0 as US-ASCII, G1 as DEC line drawing
+	startAltChars     = "\x0e"                               // aka Shift-Out
+	endAltChars       = "\x0f"                               // aka Shift-In
+	setFg8            = "\x1b[3%dm"                          // for colors less than 8
+	setFg256          = "\x1b[38;5;%dm"                      // for colors less than 256
+	setFgRgb          = "\x1b[38;2;%d;%d;%dm"                // for RGB
+	setBg8            = "\x1b[4%dm"                          // color colors less than 8
+	setBg256          = "\x1b[48;5;%dm"                      // for colors less than 256
+	setBgRgb          = "\x1b[48;2;%d;%d;%dm"                // for RGB
+	setFgBg256        = "\x1b[38;5;%d;;48;5;%dm"             // for colors less than 256, in one shot
+	setFgBgRgb        = "\x1b[38;2;%d;%d;%d;;48;2;%d;%d;%dm" // for colors less than 256, in one shot
+	resetFgBg         = "\x1b[39;49m"
 )
 
 // NewTerminfoScreenFromTtyTerminfo returns a Screen using a custom Tty
@@ -108,6 +118,12 @@ func NewTerminfoScreenFromTtyTerminfo(tty Tty, ti *terminfo.Terminfo) (s Screen,
 	if term == "" {
 		term = os.Getenv("TERM")
 	}
+	addTrueColor := false
+	switch os.Getenv("COLORTERM") {
+	case "truecolor", "24bit", "24-bit":
+		addTrueColor = true
+	}
+
 	if ti == nil {
 		// As a default, we assume XTerm like semantics, which works for the *vast*
 		// majority of terminals.
@@ -120,6 +136,7 @@ func NewTerminfoScreenFromTtyTerminfo(tty Tty, ti *terminfo.Terminfo) (s Screen,
 			switch {
 			case term == "xterm-direct":
 			case strings.Contains(term, "direct"), strings.Contains(term, "truecolor"), strings.Contains(term, "24bit"):
+				addTrueColor = true
 				term = "xterm-direct"
 			case strings.Contains(term, "256color"):
 				term = "xterm-256color"
@@ -134,6 +151,9 @@ func NewTerminfoScreenFromTtyTerminfo(tty Tty, ti *terminfo.Terminfo) (s Screen,
 	}
 
 	t := &tScreen{ti: ti, tty: tty}
+	if addTrueColor {
+		t.truecolor = true
+	}
 
 	if len(ti.Mouse) > 0 {
 		t.mouse = []byte(ti.Mouse)
@@ -237,7 +257,7 @@ func (t *tScreen) Init() error {
 	if i, _ := strconv.Atoi(os.Getenv("COLUMNS")); i != 0 {
 		w = i
 	}
-	if t.ti.SetFgBgRGB != "" || t.ti.SetFgRGB != "" || t.ti.SetBgRGB != "" {
+	if t.nColors() > 256 {
 		t.truecolor = true
 	}
 	// A user who wants to have his themes honored can
@@ -397,7 +417,6 @@ func (t *tScreen) encodeStr(s string) []byte {
 }
 
 func (t *tScreen) sendFgBg(fg Color, bg Color, attr AttrMask) AttrMask {
-	ti := t.ti
 	if t.Colors() == 0 {
 		// foreground vs background, we calculate luminance
 		// and possibly do a reverse video
@@ -418,28 +437,25 @@ func (t *tScreen) sendFgBg(fg Color, bg Color, attr AttrMask) AttrMask {
 	}
 
 	if fg == ColorReset || bg == ColorReset {
-		t.TPuts(ti.ResetFgBg)
+		t.TPuts(resetFgBg)
 	}
 	if t.truecolor {
-		if ti.SetFgBgRGB != "" && fg.IsRGB() && bg.IsRGB() {
+		if fg.IsRGB() && bg.IsRGB() {
 			r1, g1, b1 := fg.RGB()
 			r2, g2, b2 := bg.RGB()
-			t.TPuts(ti.TParm(ti.SetFgBgRGB,
-				int(r1), int(g1), int(b1),
-				int(r2), int(g2), int(b2)))
+			t.TPuts(fmt.Sprintf(setFgBgRgb, r1, g1, b1, r2, g2, b2))
 			return attr
 		}
 
-		if fg.IsRGB() && ti.SetFgRGB != "" {
+		if fg.IsRGB() {
 			r, g, b := fg.RGB()
-			t.TPuts(ti.TParm(ti.SetFgRGB, int(r), int(g), int(b)))
+			t.TPuts(fmt.Sprintf(setFgRgb, r, g, b))
 			fg = ColorDefault
 		}
 
-		if bg.IsRGB() && ti.SetBgRGB != "" {
+		if bg.IsRGB() {
 			r, g, b := bg.RGB()
-			t.TPuts(ti.TParm(ti.SetBgRGB,
-				int(r), int(g), int(b)))
+			t.TPuts(fmt.Sprintf(setBgRgb, r, g, b))
 			bg = ColorDefault
 		}
 	}
@@ -464,14 +480,21 @@ func (t *tScreen) sendFgBg(fg Color, bg Color, attr AttrMask) AttrMask {
 		}
 	}
 
-	if fg.Valid() && bg.Valid() && ti.SetFgBg != "" {
-		t.TPuts(ti.TParm(ti.SetFgBg, int(fg&0xff), int(bg&0xff)))
-	} else {
-		if fg.Valid() && ti.SetFg != "" {
-			t.TPuts(ti.TParm(ti.SetFg, int(fg&0xff)))
+	if fg.Valid() && bg.Valid() && t.nColors() > 8 {
+		t.TPuts(fmt.Sprintf(setFgBg256, fg&0xff, bg&0xff))
+	} else if t.nColors() > 8 {
+		if fg.Valid() {
+			t.TPuts(fmt.Sprintf(setFg256, fg&0xff))
 		}
-		if bg.Valid() && ti.SetBg != "" {
-			t.TPuts(ti.TParm(ti.SetBg, int(bg&0xff)))
+		if bg.Valid() {
+			t.TPuts(fmt.Sprintf(setBg256, bg&0xff))
+		}
+	} else if t.nColors() > 0 {
+		if fg.Valid() {
+			t.TPuts(fmt.Sprintf(setFg8, fg&0x7))
+		}
+		if bg.Valid() {
+			t.TPuts(fmt.Sprintf(setBg8, bg&0x7))
 		}
 	}
 	return attr
@@ -1155,7 +1178,7 @@ func (t *tScreen) disengage() {
 		t.TPuts(t.cursorFg)
 	}
 	t.TPuts(ti.ExitKeypad)
-	t.TPuts(ti.ResetFgBg)
+	t.TPuts(resetFgBg)
 	t.TPuts(sgr0)
 	t.TPuts(enableAutoMargin)
 	t.TPuts(t.disableCsiU)

terminfo/a/aixterm/term.go
@@ -8,13 +8,9 @@ func init() {
 
 	// IBM Aixterm Terminal Emulator
 	terminfo.AddTerminfo(&terminfo.Terminfo{
-		Name:      "aixterm",
-		Columns:   80,
-		Lines:     25,
-		Colors:    8,
-		SetFg:     "\x1b[3%p1%dm",
-		SetBg:     "\x1b[4%p1%dm",
-		SetFgBg:   "\x1b[3%p1%d;4%p2%dm",
-		ResetFgBg: "\x1b[32m\x1b[40m",
+		Name:    "aixterm",
+		Columns: 80,
+		Lines:   25,
+		Colors:  8,
 	})
 }

terminfo/a/ansi/term.go
@@ -8,14 +8,10 @@ func init() {
 
 	// ansi/pc-term compatible with color
 	terminfo.AddTerminfo(&terminfo.Terminfo{
-		Name:      "ansi",
-		Aliases:   []string{"pcansi"},
-		Columns:   80,
-		Lines:     24,
-		Colors:    8,
-		SetFg:     "\x1b[3%p1%dm",
-		SetBg:     "\x1b[4%p1%dm",
-		SetFgBg:   "\x1b[3%p1%d;4%p2%dm",
-		ResetFgBg: "\x1b[39;49m",
+		Name:    "ansi",
+		Aliases: []string{"pcansi"},
+		Columns: 80,
+		Lines:   24,
+		Colors:  8,
 	})
 }

terminfo/d/dtterm/term.go
@@ -8,13 +8,9 @@ func init() {
 
 	// CDE desktop terminal
 	terminfo.AddTerminfo(&terminfo.Terminfo{
-		Name:      "dtterm",
-		Columns:   80,
-		Lines:     24,
-		Colors:    8,
-		SetFg:     "\x1b[3%p1%dm",
-		SetBg:     "\x1b[4%p1%dm",
-		SetFgBg:   "\x1b[3%p1%d;4%p2%dm",
-		ResetFgBg: "\x1b[39;49m",
+		Name:    "dtterm",
+		Columns: 80,
+		Lines:   24,
+		Colors:  8,
 	})
 }

terminfo/l/linux/term.go
@@ -8,12 +8,8 @@ func init() {
 
 	// Linux console
 	terminfo.AddTerminfo(&terminfo.Terminfo{
-		Name:      "linux",
-		Colors:    8,
-		SetFg:     "\x1b[3%p1%dm",
-		SetBg:     "\x1b[4%p1%dm",
-		SetFgBg:   "\x1b[3%p1%d;4%p2%dm",
-		ResetFgBg: "\x1b[39;49m",
-		Mouse:     "\x1b[M",
+		Name:   "linux",
+		Colors: 8,
+		Mouse:  "\x1b[M",
 	})
 }

terminfo/mkinfo.go
@@ -39,7 +39,6 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"regexp"
 	"strconv"
 	"strings"
 
@@ -218,9 +217,6 @@ func getinfo(name string) (*terminfo.Terminfo, string, error) {
 	t.ExitCA = tc.getstr("rmcup")
 	t.EnterKeypad = tc.getstr("smkx")
 	t.ExitKeypad = tc.getstr("rmkx")
-	t.SetFg = tc.getstr("setaf")
-	t.SetBg = tc.getstr("setab")
-	t.ResetFgBg = tc.getstr("op")
 	t.Mouse = tc.getstr("kmous")
 
 	// Technically the RGB flag that is provided for xterm-direct is not
@@ -237,25 +233,11 @@ func getinfo(name string) (*terminfo.Terminfo, string, error) {
 		// (ncurses went a very different direction from everyone else, and
 		// so it's unlikely anything is using this definition.)
 		t.TrueColor = true
-		t.SetBg = "\x1b[%?%p1%{8}%<%t4%p1%d%e%p1%{16}%<%t10%p1%{8}%-%d%e48;5;%p1%d%;m"
-		t.SetFg = "\x1b[%?%p1%{8}%<%t3%p1%d%e%p1%{16}%<%t9%p1%{8}%-%d%e38;5;%p1%d%;m"
 	}
 
 	// We only support colors in ANSI 8 or 256 color mode.
-	if t.Colors < 8 || t.SetFg == "" {
+	if t.Colors < 8 {
 		t.Colors = 0
-	}
-
-	// For terminals that use "standard" SGR sequences, lets combine the
-	// foreground and background together.
-	if strings.HasPrefix(t.SetFg, "\x1b[") &&
-		strings.HasPrefix(t.SetBg, "\x1b[") &&
-		strings.HasSuffix(t.SetFg, "m") &&
-		strings.HasSuffix(t.SetBg, "m") {
-		fg := t.SetFg[:len(t.SetFg)-1]
-		r := regexp.MustCompile("%p1")
-		bg := r.ReplaceAllString(t.SetBg[2:], "%p2")
-		t.SetFgBg = fg + ";" + bg
 	}
 
 	if tc.getflag("XT") {
@@ -328,13 +310,6 @@ func dotGoInfo(w io.Writer, terms []*TData) {
 		dotGoAddStr(w, "ExitCA", t.ExitCA)
 		dotGoAddStr(w, "EnterKeypad", t.EnterKeypad)
 		dotGoAddStr(w, "ExitKeypad", t.ExitKeypad)
-		dotGoAddStr(w, "SetFg", t.SetFg)
-		dotGoAddStr(w, "SetBg", t.SetBg)
-		dotGoAddStr(w, "SetFgBg", t.SetFgBg)
-		dotGoAddStr(w, "ResetFgBg", t.ResetFgBg)
-		dotGoAddStr(w, "SetFgRGB", t.SetFgRGB)
-		dotGoAddStr(w, "SetBgRGB", t.SetBgRGB)
-		dotGoAddStr(w, "SetFgBgRGB", t.SetFgBgRGB)
 		dotGoAddStr(w, "Mouse", t.Mouse)
 		dotGoAddFlag(w, "TrueColor", t.TrueColor)
 		dotGoAddFlag(w, "XTermLike", t.XTermLike)

terminfo/terminfo.go
@@ -19,7 +19,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -50,9 +49,6 @@ type Terminfo struct {
 	ExitCA      string // rmcup
 	EnterKeypad string // smkx
 	ExitKeypad  string // rmkx
-	SetFg       string // setaf
-	SetBg       string // setab
-	ResetFgBg   string // op
 	Mouse       string // kmous
 
 	// These are non-standard extensions to terminfo.  This includes
@@ -61,12 +57,8 @@ type Terminfo struct {
 	// Terminal support for these are going to vary amongst XTerm
 	// emulations, so don't depend too much on them in your application.
 
-	SetFgBg    string // setfgbg
-	SetFgBgRGB string // setfgbgrgb
-	SetFgRGB   string // setfrgb
-	SetBgRGB   string // setbrgb
-	TrueColor  bool   // true if the terminal supports direct color
-	XTermLike  bool   // (XT) has XTerm extensions
+	TrueColor bool // true if the terminal supports direct color
+	XTermLike bool // (XT) has XTerm extensions
 }
 
 type stack []any
@@ -452,31 +444,6 @@ func (t *Terminfo) TPuts(w io.Writer, s string) {
 	}
 }
 
-// TColor returns a string corresponding to the given foreground and background
-// colors.  Either fg or bg can be set to -1 to elide.
-func (t *Terminfo) TColor(fi, bi int) string {
-	rv := ""
-	// As a special case, we map bright colors to lower versions if the
-	// color table only holds 8.  For the remaining 240 colors, the user
-	// is out of luck.  Someday we could create a mapping table, but its
-	// not worth it.
-	if t.Colors == 8 {
-		if fi > 7 && fi < 16 {
-			fi -= 8
-		}
-		if bi > 7 && bi < 16 {
-			bi -= 8
-		}
-	}
-	if t.Colors > fi && fi >= 0 {
-		rv += t.TParm(t.SetFg, fi)
-	}
-	if t.Colors > bi && bi >= 0 {
-		rv += t.TParm(t.SetBg, bi)
-	}
-	return rv
-}
-
 var (
 	dblock    sync.Mutex
 	terminfos = make(map[string]*Terminfo)
@@ -501,85 +468,12 @@ func LookupTerminfo(name string) (*Terminfo, error) {
 		return nil, ErrTermNotFound
 	}
 
-	addtruecolor := false
-	add256color := false
-	switch os.Getenv("COLORTERM") {
-	case "truecolor", "24bit", "24-bit":
-		addtruecolor = true
-	}
 	dblock.Lock()
 	t := terminfos[name]
 	dblock.Unlock()
 
-	// If the name ends in -truecolor, then fabricate an entry
-	// from the corresponding -256color, -color, or bare terminal.
-	if t != nil && t.TrueColor {
-		addtruecolor = true
-	} else if t == nil && strings.HasSuffix(name, "-truecolor") {
-
-		suffixes := []string{
-			"-256color",
-			"-88color",
-			"-color",
-			"",
-		}
-		base := name[:len(name)-len("-truecolor")]
-		for _, s := range suffixes {
-			if t, _ = LookupTerminfo(base + s); t != nil {
-				addtruecolor = true
-				break
-			}
-		}
-	}
-
-	// If the name ends in -256color, maybe fabricate using the xterm 256 color sequences
-	if t == nil && strings.HasSuffix(name, "-256color") {
-		suffixes := []string{
-			"-88color",
-			"-color",
-		}
-		base := name[:len(name)-len("-256color")]
-		for _, s := range suffixes {
-			if t, _ = LookupTerminfo(base + s); t != nil {
-				add256color = true
-				break
-			}
-		}
-	}
-
 	if t == nil {
 		return nil, ErrTermNotFound
-	}
-
-	switch os.Getenv("TCELL_TRUECOLOR") {
-	case "":
-	case "disable":
-		addtruecolor = false
-	default:
-		addtruecolor = true
-	}
-
-	// If the user has requested 24-bit color with $COLORTERM, then
-	// amend the value (unless already present).  This means we don't
-	// need to have a value present.
-	if addtruecolor &&
-		t.SetFgBgRGB == "" &&
-		t.SetFgRGB == "" &&
-		t.SetBgRGB == "" {
-
-		// Supply vanilla ISO 8613-6:1994 24-bit color sequences.
-		t.SetFgRGB = "\x1b[38;2;%p1%d;%p2%d;%p3%dm"
-		t.SetBgRGB = "\x1b[48;2;%p1%d;%p2%d;%p3%dm"
-		t.SetFgBgRGB = "\x1b[38;2;%p1%d;%p2%d;%p3%d;" +
-			"48;2;%p4%d;%p5%d;%p6%dm"
-	}
-
-	if add256color {
-		t.Colors = 256
-		t.SetFg = "\x1b[%?%p1%{8}%<%t3%p1%d%e%p1%{16}%<%t9%p1%{8}%-%d%e38;5;%p1%d%;m"
-		t.SetBg = "\x1b[%?%p1%{8}%<%t4%p1%d%e%p1%{16}%<%t10%p1%{8}%-%d%e48;5;%p1%d%;m"
-		t.SetFgBg = "\x1b[%?%p1%{8}%<%t3%p1%d%e%p1%{16}%<%t9%p1%{8}%-%d%e38;5;%p1%d%;;%?%p2%{8}%<%t4%p2%d%e%p2%{16}%<%t10%p2%{8}%-%d%e48;5;%p2%d%;m"
-		t.ResetFgBg = "\x1b[39;49m"
 	}
 
 	return t, nil

terminfo/terminfo_test.go
@@ -25,8 +25,6 @@ var testTerminfo = &Terminfo{
 	Columns:   80,
 	Lines:     24,
 	Colors:    256,
-	SetFg:     "\x1b[%?%p1%{8}%<%t3%p1%d%e%p1%{16}%<%t9%p1%{8}%-%d%e38;5;%p1%d%;m",
-	SetBg:     "\x1b[%?%p1%{8}%<%t4%p1%d%e%p1%{16}%<%t10%p1%{8}%-%d%e48;5;%p1%d%;m",
 	Mouse:     "\x1b[M",
 	XTermLike: true,
 }
@@ -40,15 +38,6 @@ func TestTerminfoExpansion(t *testing.T) {
 	}
 
 	// Color tests.
-	if ti.TParm(ti.SetFg, 7) != "\x1b[37m" {
-		t.Error("SetFg(7) failed")
-	}
-	if ti.TParm(ti.SetFg, 15) != "\x1b[97m" {
-		t.Error("SetFg(15) failed")
-	}
-	if ti.TParm(ti.SetFg, 200) != "\x1b[38;5;200m" {
-		t.Error("SetFg(200) failed")
-	}
 
 	type testCase struct {
 		expect string
@@ -113,14 +102,5 @@ func TestStringParameter(t *testing.T) {
 	s = ti.TParm(enterUrl, "https://example.org/test", "id=1234")
 	if s != "\x1b]8;id=1234;https://example.org/test\x1b\\" {
 		t.Errorf("Result string failed: %s", s)
-	}
-}
-
-func BenchmarkSetFgBg(b *testing.B) {
-	ti := testTerminfo
-
-	for b.Loop() {
-		ti.TParm(ti.SetFg, 100, 200)
-		ti.TParm(ti.SetBg, 100, 200)
 	}
 }

commit 49ff790ad1ab67b56eff1060d37e79262fd83ecf

Remove extra semicolon from setFgBgRgb
Foreground color was not set when both fg and bg were RGB colors.

The bug was introduced in:

	5c18b2ff29ccbc80703ef93ea78af6e2e6c6caf5
	chore: refactor color support to use ECMA SGR universally

tscreen.go
@@ -92,7 +92,7 @@ const (
 	setBg8            = "\x1b[4%dm"                          // color colors less than 8
 	setBg256          = "\x1b[48;5;%dm"                      // for colors less than 256
 	setBgRgb          = "\x1b[48;2;%d;%d;%dm"                // for RGB
-	setFgBgRgb        = "\x1b[38;2;%d;%d;%d;;48;2;%d;%d;%dm" // for colors less than 256, in one shot
+	setFgBgRgb        = "\x1b[38;2;%d;%d;%d;48;2;%d;%d;%dm"  // for RGB, in one shot
 	resetFgBg         = "\x1b[39;49m"                        // ECMA defined
 	enterCA           = "\x1b[?1049h"                        // alternate screen
 	exitCA            = "\x1b[?1049l"                        // alternate screen