393ed0bf79ed0b4523c7215639cdf79580b99472
Garrett D'Amore <garrett@damore.org>
2025-12-10 19:25:18 -0800

    fix: fix terminal key protocol selection logic
    The logic to force behavior for WezTerm was wrong, and was the
    result of an incorrect escape sequence for the win32-input-mode.
    Fixing that lets us eliminate the WezTerm specific logic.

--- tscreen.go
+++ tscreen.go
@@ -300,23 +300,17 @@ func (t *tScreen) prepareExtendedOSC() {
 	// are not as widely deployed, and OSC 9 is not unique.)
 	t.notifyDesktop = "\x1b]777;notify;%s;%s\x1b\\"
 
-	if t.enableCsiU == "" {
-		if runtime.GOOS == "windows" && (os.Getenv("TERM") == "" || os.Getenv("TERM_PROGRAM") == "WezTerm") {
-			// on Windows, if we don't have a TERM, use only win32-input-mode
-			t.enableCsiU = "\x1b[?9001h"
-			t.disableCsiU = "\x1b[?9001l"
-		} else if os.Getenv("TERM_PROGRAM") == "WezTerm" {
-			// WezTerm is unhappy if we ask for other modes
-			t.enableCsiU = "\x1b[>1u"
-			t.disableCsiU = "\x1b[<u"
-		} else {
-			// three advanced keyboard protocols:
-			// - xterm modifyOtherKeys (uses CSI 27 ~ )
-			// - kitty csi-u (uses CSI u)
-			// - win32-input-mode (uses CSI _)
-			t.enableCsiU = "\x1b[>4;2m" + "\x1b[>1u" + "\x1b[9001h"
-			t.disableCsiU = "\x1b[9001l" + "\x1b[<u" + "\x1b[>4;0m"
-		}
+	if runtime.GOOS == "windows" && os.Getenv("TERM") == "" {
+		// on Windows, if we don't have a TERM, use only win32-input-mode
+		t.enableCsiU = "\x1b[?9001h"
+		t.disableCsiU = "\x1b[?9001l"
+	} else {
+		// three advanced keyboard protocols:
+		// - xterm modifyOtherKeys (uses CSI 27 ~ )
+		// - kitty csi-u (uses CSI u)
+		// - win32-input-mode (uses CSI _)
+		t.enableCsiU = "\x1b[>4;2m" + "\x1b[>1u" + "\x1b[?9001h"
+		t.disableCsiU = "\x1b[?9001l" + "\x1b[<u" + "\x1b[>4;0m"
 	}
 }
 
