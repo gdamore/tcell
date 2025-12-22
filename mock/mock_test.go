be32775bea0a26de5582c1083556098141fa9a9f
Garrett D'Amore <garrett@damore.org>
2025-12-21 18:34:52 -0800

    chore: More mock related improvements.
    This adds testing of the cursor movement primitives, and better
    synchronization for the drain operation, as well as support for DECALN
    and a supporting test case.

--- mock/mock_test.go
+++ mock/mock_test.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/gdamore/tcell/v3"
+	"github.com/gdamore/tcell/v3/vt"
 )
 
 // TestMockStart just verifies that we can start the mock tty.
@@ -42,4 +43,110 @@ func TestMockStart(t *testing.T) {
 	}
 	time.Sleep(time.Millisecond * 100)
 	screen.Fini()
+}
+
+func TestMockDECALN(t *testing.T) {
+	mt := &MockTty{Rows: 3, Cols: 5, Fg: tcell.ColorWhite, Bg: tcell.ColorBlack}
+	mt.Reset()
+	if err := mt.Start(); err != nil {
+		t.Fatalf("Failed to start: %v", err)
+	}
+	mt.Write([]byte("\x1b#8"))
+	mt.Drain()
+	mt.Stop()
+	if len(mt.Cells) != 15 {
+		t.Fatalf("Wrong cell length: %d", len(mt.Cells))
+	}
+	for i := range mt.Cells {
+		if string(mt.Cells[i].C) != "E" {
+			t.Errorf("wrong value at %d: %s", i, string(mt.Cells[i].C))
+			break
+		}
+		if mt.Cells[i].Attr != tcell.AttrNone {
+			t.Errorf("wrong attr at %d: %x", i, mt.Cells[i].Attr)
+		}
+		if mt.Cells[i].Fg != tcell.ColorWhite {
+			t.Errorf("wrong fg at %d: %s", i, mt.Cells[i].Fg.String())
+		}
+		if mt.Cells[i].Bg != tcell.ColorBlack {
+			t.Errorf("wrong bg at %d: %s", i, mt.Cells[i].Bg.String())
+		}
+		if mt.Cells[i].Width != 1 {
+			t.Errorf("wrong width at %d: %d", i, mt.Cells[i].Width)
+		}
+
+	}
+	if err := mt.Start(); err != nil {
+		t.Fatalf("Failed to start: %v", err)
+	}
+	mt.Fg = tcell.ColorRed
+	mt.Attr = tcell.AttrBold
+	mt.Write([]byte("\x1b#8"))
+	mt.Drain()
+	for i := range mt.Cells {
+		if string(mt.Cells[i].C) != "E" {
+			t.Errorf("wrong value at %d: %s", i, string(mt.Cells[i].C))
+			break
+		}
+		if mt.Cells[i].Attr != tcell.AttrBold {
+			t.Errorf("wrong attr at %d: %x", i, mt.Cells[i].Attr)
+		}
+		if mt.Cells[i].Fg != tcell.ColorRed {
+			t.Errorf("wrong fg at %d: %s", i, mt.Cells[i].Fg.String())
+		}
+		if mt.Cells[i].Bg != tcell.ColorBlack {
+			t.Errorf("wrong bg at %d: %s", i, mt.Cells[i].Bg.String())
+		}
+	}
+	mt.Stop()
+
+	mt.Close()
+}
+
+func TestMockCursorMovement(t *testing.T) {
+	mt := &MockTty{Rows: 3, Cols: 5, Fg: tcell.ColorWhite, Bg: tcell.ColorBlack}
+	mt.Reset()
+	if err := mt.Start(); err != nil {
+		t.Fatalf("Failed to start: %v", err)
+	}
+	checkPos := func(x vt.Col, y vt.Row) {
+		mt.Drain()
+		if mt.X != x || mt.Y != y {
+			t.Errorf("bad position %d,%d (expected %d,%d)", mt.X, mt.Y, x, y)
+		}
+	}
+	mt.Write([]byte("\x1b[2;3H"))
+	checkPos(2, 1)
+
+	mt.Write([]byte("\x1b[20A"))
+	checkPos(2, 0)
+
+	mt.Write([]byte("\x1b[20B"))
+	checkPos(2, 2)
+
+	mt.Write([]byte("\x1b[A"))
+	checkPos(2, 1)
+
+	mt.Write([]byte("\x1b[2C"))
+	checkPos(4, 1)
+
+	mt.Write([]byte("\x1b[3D"))
+	checkPos(1, 1)
+
+	mt.Write([]byte("\x1b[100D"))
+	checkPos(0, 1)
+
+	// Now try the next line and previous line
+	mt.Write([]byte("\x1b[2;3H"))
+	checkPos(2, 1)
+	mt.Write([]byte("\x1b[1E"))
+	checkPos(0, 2)
+
+	mt.Write([]byte("\x1b[2;3H"))
+	checkPos(2, 1)
+	mt.Write([]byte("\x1b[1F"))
+	checkPos(0, 0)
+
+	mt.Stop()
+	mt.Close()
 }
