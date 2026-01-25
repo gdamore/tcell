515dfacb061f7559631e9e52584e1cf229fe2f7b
Garrett D'Amore <garrett@damore.org>
2026-01-25 12:53:16 -0800

    refactor(emulator): Reduce allocations (thanks CodeRabbit) by hoisting.
    Also the VKey() method for Key was incomplete and incorrect semantically
    as we need to have layout information to properly determine the virtual key.
    So it is removed now.

--- vt/key.go
+++ vt/key.go
@@ -205,140 +205,143 @@ const (
 	KeyRHyper       = Key(0xff02) // software only, in reserved space
 )
 
+// scanCodes is a list of Windows scan codes for physical keys.
+var scanCodes = map[Key]ScanCode{
+	KeyA:            0x1e,
+	KeyB:            0x30,
+	KeyC:            0x2e,
+	KeyD:            0x20,
+	KeyE:            0x12,
+	KeyF:            0x21,
+	KeyG:            0x22,
+	KeyH:            0x23,
+	KeyI:            0x17,
+	KeyJ:            0x24,
+	KeyK:            0x25,
+	KeyL:            0x26,
+	KeyM:            0x32,
+	KeyN:            0x31,
+	KeyO:            0x18,
+	KeyP:            0x19,
+	KeyQ:            0x10,
+	KeyR:            0x13,
+	KeyS:            0x1f,
+	KeyT:            0x14,
+	KeyU:            0x16,
+	KeyV:            0x2f,
+	KeyW:            0x11,
+	KeyX:            0x2d,
+	KeyY:            0x15,
+	KeyZ:            0x2c,
+	Key1:            0x02,
+	Key2:            0x03,
+	Key3:            0x04,
+	Key4:            0x05,
+	Key5:            0x06,
+	Key6:            0x07,
+	Key7:            0x08,
+	Key8:            0x09,
+	Key9:            0x0a,
+	Key0:            0x0b,
+	KeyEnter:        0x1c,
+	KeyEsc:          0x01,
+	KeyBackspace:    0x0e,
+	KeyTab:          0x0f,
+	KeySpace:        0x39,
+	KeyMinus:        0x0c,
+	KeyEqual:        0x0d,
+	KeyLBrace:       0x1a,
+	KeyRBrace:       0x1b,
+	KeyBackslash:    0x2b,
+	KeySemi:         0x27,
+	KeyQuote:        0x28,
+	KeyGrave:        0x29,
+	KeyComma:        0x33,
+	KeyPeriod:       0x34,
+	KeySlash:        0x35,
+	KeyCapsLock:     0x3a,
+	KeyF1:           0x3b,
+	KeyF2:           0x3c,
+	KeyF3:           0x3d,
+	KeyF4:           0x3e,
+	KeyF5:           0x3f,
+	KeyF6:           0x40,
+	KeyF7:           0x41,
+	KeyF8:           0x42,
+	KeyF9:           0x43,
+	KeyF10:          0x44,
+	KeyF11:          0x57,
+	KeyF12:          0x58,
+	KeyPrtScr:       0x54,
+	KeyScrLock:      0x46,
+	KeyPause:        0xe046,
+	KeyInsert:       0xe052,
+	KeyHome:         0xe047,
+	KeyPgUp:         0xe049,
+	KeyDelete:       0xe053,
+	KeyEnd:          0xe04f,
+	KeyPgDn:         0xe051,
+	KeyRight:        0xe04d,
+	KeyLeft:         0xe04b,
+	KeyDown:         0xe050,
+	KeyUp:           0xe048,
+	KeyNumLock:      0x45,
+	KeyPadDiv:       0xe035,
+	KeyPadMul:       0x37,
+	KeyPadSub:       0x4a,
+	KeyPadAdd:       0x4e,
+	KeyPadEnter:     0xe01c,
+	KeyPad1:         0x4f,
+	KeyPad2:         0x50,
+	KeyPad3:         0x51,
+	KeyPad4:         0x4b,
+	KeyPad5:         0x4c,
+	KeyPad6:         0x4d,
+	KeyPad7:         0x47,
+	KeyPad8:         0x48,
+	KeyPad9:         0x49,
+	KeyPad0:         0x52,
+	KeyPadDec:       0x53,
+	KeyIsoBackSlash: 0x56,
+	KeyPadEqual:     0x59,
+	KeyF13:          0x64,
+	KeyF14:          0x65,
+	KeyF15:          0x66,
+	KeyF16:          0x67,
+	KeyF17:          0x68,
+	KeyF18:          0x69,
+	KeyF19:          0x6a,
+	KeyF20:          0x6b,
+	KeyF21:          0x6c,
+	KeyF22:          0x6d,
+	KeyF23:          0x6e,
+	KeyF24:          0x76,
+	KeyPadComma:     0x7e,
+	KeyIsoSlash:     0x73,
+	KeyHiragana:     0x70,
+	KeyYen:          0x7d,
+	KeyConvert:      0x79,
+	KeyNonConvert:   0x7b,
+	KeyLCtrl:        0x1d,
+	KeyLShift:       0x2a,
+	KeyLAlt:         0x38,
+	KeyLMeta:        0xe05b,
+	KeyRCtrl:        0xe01d,
+	KeyRShift:       0x36,
+	KeyRAlt:         0xe038,
+	KeyRMeta:        0xe05c,
+	KeyMenu:         0xe05d,
+
+	// Windows uses a bunch of HID usages from
+	// the consumer page (0x0c) for media playback, and
+	// other applications. We just ignore them.
+}
+
 // ScanCode returns the corresponding Windows Scan Code (not a VK!)
 // for the given key. (Virtual keys should be determined by the
 // host OS using the current keyboard layout.)
 func (k Key) ScanCode() ScanCode {
-	var scanCodes = map[Key]ScanCode{
-		KeyA:            0x1e,
-		KeyB:            0x30,
-		KeyC:            0x2e,
-		KeyD:            0x20,
-		KeyE:            0x12,
-		KeyF:            0x21,
-		KeyG:            0x22,
-		KeyH:            0x23,
-		KeyI:            0x17,
-		KeyJ:            0x24,
-		KeyK:            0x25,
-		KeyL:            0x26,
-		KeyM:            0x32,
-		KeyN:            0x31,
-		KeyO:            0x18,
-		KeyP:            0x19,
-		KeyQ:            0x10,
-		KeyR:            0x13,
-		KeyS:            0x1f,
-		KeyT:            0x14,
-		KeyU:            0x16,
-		KeyV:            0x2f,
-		KeyW:            0x11,
-		KeyX:            0x2d,
-		KeyY:            0x15,
-		KeyZ:            0x2c,
-		Key1:            0x02,
-		Key2:            0x03,
-		Key3:            0x04,
-		Key4:            0x05,
-		Key5:            0x06,
-		Key6:            0x07,
-		Key7:            0x08,
-		Key8:            0x09,
-		Key9:            0x0a,
-		Key0:            0x0b,
-		KeyEnter:        0x1c,
-		KeyEsc:          0x01,
-		KeyBackspace:    0x0e,
-		KeyTab:          0x0f,
-		KeySpace:        0x39,
-		KeyMinus:        0x0c,
-		KeyEqual:        0x0d,
-		KeyLBrace:       0x1a,
-		KeyRBrace:       0x1b,
-		KeyBackslash:    0x2b,
-		KeySemi:         0x27,
-		KeyQuote:        0x28,
-		KeyGrave:        0x29,
-		KeyComma:        0x33,
-		KeyPeriod:       0x34,
-		KeySlash:        0x35,
-		KeyCapsLock:     0x3a,
-		KeyF1:           0x3b,
-		KeyF2:           0x3c,
-		KeyF3:           0x3d,
-		KeyF4:           0x3e,
-		KeyF5:           0x3f,
-		KeyF6:           0x40,
-		KeyF7:           0x41,
-		KeyF8:           0x42,
-		KeyF9:           0x43,
-		KeyF10:          0x44,
-		KeyF11:          0x57,
-		KeyF12:          0x58,
-		KeyPrtScr:       0x54,
-		KeyScrLock:      0x46,
-		KeyPause:        0xe046,
-		KeyInsert:       0xe052,
-		KeyHome:         0xe047,
-		KeyPgUp:         0xe049,
-		KeyDelete:       0xe053,
-		KeyEnd:          0xe04f,
-		KeyPgDn:         0xe051,
-		KeyRight:        0xe04d,
-		KeyLeft:         0xe04b,
-		KeyDown:         0xe050,
-		KeyUp:           0xe048,
-		KeyNumLock:      0x45,
-		KeyPadDiv:       0xe035,
-		KeyPadMul:       0x37,
-		KeyPadSub:       0x4a,
-		KeyPadAdd:       0x4e,
-		KeyPadEnter:     0xe01c,
-		KeyPad1:         0x4f,
-		KeyPad2:         0x50,
-		KeyPad3:         0x51,
-		KeyPad4:         0x4b,
-		KeyPad5:         0x4c,
-		KeyPad6:         0x4d,
-		KeyPad7:         0x47,
-		KeyPad8:         0x48,
-		KeyPad9:         0x49,
-		KeyPad0:         0x52,
-		KeyPadDec:       0x53,
-		KeyIsoBackSlash: 0x56,
-		KeyPadEqual:     0x59,
-		KeyF13:          0x64,
-		KeyF14:          0x65,
-		KeyF15:          0x66,
-		KeyF16:          0x67,
-		KeyF17:          0x68,
-		KeyF18:          0x69,
-		KeyF19:          0x6a,
-		KeyF20:          0x6b,
-		KeyF21:          0x6c,
-		KeyF22:          0x6d,
-		KeyF23:          0x6e,
-		KeyF24:          0x76,
-		KeyPadComma:     0x7e,
-		KeyIsoSlash:     0x73,
-		KeyHiragana:     0x70,
-		KeyYen:          0x7d,
-		KeyConvert:      0x79,
-		KeyNonConvert:   0x7b,
-		KeyLCtrl:        0x1d,
-		KeyLShift:       0x2a,
-		KeyLAlt:         0x38,
-		KeyLMeta:        0xe05b,
-		KeyRCtrl:        0xe01d,
-		KeyRShift:       0x36,
-		KeyRAlt:         0xe038,
-		KeyRMeta:        0xe05c,
-		KeyMenu:         0xe05d,
 
-		// Windows uses a bunch of HID usages from
-		// the consumer page (0x0c) for media playback, and
-		// other applications. We just ignore them.
-	}
 	if w, ok := scanCodes[k]; ok {
 		return w
 	}
@@ -492,146 +495,115 @@ const (
 	VkPacket     = WinVK(0xe7)
 )
 
-func (k Key) VKey() WinVK {
-	var keys = map[Key]WinVK{
-		// KeyUTF          = Key(0x01) // virtual UTF-8 content
-		KeyA: VkA,
-		KeyB: VkB,
-		KeyC: VkC,
-		KeyD: VkD,
-		KeyE: VkE,
-		KeyF: VkF,
-		KeyG: VkG,
-		KeyH: VkH,
-		KeyI: VkI,
-		KeyJ: VkJ,
-		KeyK: VkK,
-		KeyL: VkL,
-		KeyM: VkM,
-		KeyN: VkN,
-		KeyO: VkO,
-		KeyP: VkP,
-		KeyQ: VkQ,
-		KeyR: VkR,
-		KeyS: VkS,
-		KeyT: VkT,
-		KeyU: VkU,
-		KeyV: VkV,
-		KeyW: VkW,
-		KeyX: VkX,
-		KeyY: VkY,
-		KeyZ: VkZ,
-		// Key1            = Key(0x1E)
-		// Key2            = Key(0x1F)
-		// Key3            = Key(0x20)
-		// Key4            = Key(0x21)
-		// Key5            = Key(0x22)
-		// Key6            = Key(0x23)
-		// Key7            = Key(0x24)
-		// Key8            = Key(0x25)
-		// Key9            = Key(0x26)
-		// Key0            = Key(0x27)
-		// KeyEnter        = Key(0x28)
-		// KeyEsc          = Key(0x29)
-		// KeyBackspace    = Key(0x2a) // sometimes called delete
-		// KeyTab          = Key(0x2b)
-		// KeySpace        = Key(0x2c)
-		// KeyMinus        = Key(0x2d) // - and _
-		// KeyEqual        = Key(0x2e) // = and +
-		// KeyLBrace       = Key(0x2f) // [ and {
-		// KeyRBrace       = Key(0x30) // ] and }
-		// KeyBackslash    = Key(0x31) // \
-		// KeyIsoHash      = Key(0x32) // international only
-		// KeySemi         = Key(0x33) // ;
-		// KeyQuote        = Key(0x34) // ' and " aka apostrophe
-		// KeyGrave        = Key(0x35) // ` and ~
-		// KeyComma        = Key(0x36) // , and <
-		// KeyPeriod       = Key(0x37) // . and >
-		// KeySlash        = Key(0x38) // / and ?
-		// KeyCapsLock     = Key(0x39)
-		// KeyF1           = Key(0x3a)
-		// KeyF2           = Key(0x3b)
-		// KeyF3           = Key(0x3c)
-		// KeyF4           = Key(0x3d)
-		// KeyF5           = Key(0x3e)
-		// KeyF6           = Key(0x3f)
-		// KeyF7           = Key(0x40)
-		// KeyF8           = Key(0x41)
-		// KeyF9           = Key(0x42)
-		// KeyF10          = Key(0x43)
-		// KeyF11          = Key(0x44)
-		// KeyF12          = Key(0x45)
-		// KeyPrtScr       = Key(0x46)
-		// KeyScrLock      = Key(0x47)
-		// KeyPause        = Key(0x48)
-		// KeyInsert       = Key(0x49)
-		// KeyHome         = Key(0x4a)
-		// KeyPgUp         = Key(0x4b)
-		// KeyDelete       = Key(0x4c) // forward delete (DEL)
-		// KeyEnd          = Key(0x4d)
-		// KeyPgDn         = Key(0x4e)
-		// KeyRight        = Key(0x4f)
-		// KeyLeft         = Key(0x50)
-		// KeyDown         = Key(0x51)
-		// KeyUp           = Key(0x52)
-		// KeyNumLock      = Key(0x53) // also clear
-		// KeyPadDiv       = Key(0x54)
-		// KeyPadMul       = Key(0x55)
-		// KeyPadSub       = Key(0x56)
-		// KeyPadAdd       = Key(0x57)
-		// KeyPadEnter     = Key(0x58)
-		// KeyPad1         = Key(0x59) // also pad end
-		// KeyPad2         = Key(0x5a) // also pad down
-		// KeyPad3         = Key(0x5b) // also pad page down
-		// KeyPad4         = Key(0x5c) // also pad left
-		// KeyPad5         = Key(0x5d)
-		// KeyPad6         = Key(0x5e) // also pad right
-		// KeyPad7         = Key(0x5f) // also pad home
-		// KeyPad8         = Key(0x60) // also pad up
-		// KeyPad9         = Key(0x61) // also pad page up
-		// KeyPad0         = Key(0x62) // also pad insert
-		// KeyPadDec       = Key(0x63) // also pad delete
-		// KeyIsoBackSlash = Key(0x64) // international keyboards only
-		// KeyPadEqual     = Key(0x67)
-		// KeyF13          = Key(0x68)
-		// KeyF14          = Key(0x69)
-		// KeyF15          = Key(0x6a)
-		// KeyF16          = Key(0x6b)
-		// KeyF17          = Key(0x6c)
-		// KeyF18          = Key(0x6d)
-		// KeyF19          = Key(0x6e)
-		// KeyF20          = Key(0x6f)
-		// KeyF21          = Key(0x70)
-		// KeyF22          = Key(0x71)
-		// KeyF23          = Key(0x72)
-		// KeyF24          = Key(0x73)
-		// KeyMenu         = Key(0x76) // might also be 0x65, KeyApplication
-		// KeyPadComma     = Key(0x85)
-		// KeyPadEqSign    = Key(0x86)
-		// KeyIsoSlash     = Key(0x87) // also International 1
-		// KeyHiragana     = Key(0x88) // also International 2
-		// KeyYen          = Key(0x89) // found on JIS keyboards
-		// KeyConvert      = Key(0x8a)
-		// KeyNonConvert   = Key(0x8b)
-		// KeyAltErase     = Key(0x99) // e.g split space-erase bar
-		// KeySysReq       = Key(0x9a)
-		// KeyCancel       = Key(0x9b)
-		// KeyLControl     = Key(0xe0)
-		// KeyLShift       = Key(0xe1)
-		// KeyLAlt         = Key(0xe2)
-		// KeyLMeta        = Key(0xe3)
-		// KeyRControl     = Key(0xe4)
-		// KeyRShift       = Key(0xe5)
-		// KeyRAlt         = Key(0xe6)
-		// KeyRMeta        = Key(0xe7)
+var baseKeys = map[Key]BaseKey{
+	KeyA:            'a',
+	KeyB:            'b',
+	KeyC:            'c',
+	KeyD:            'd',
+	KeyE:            'e',
+	KeyF:            'f',
+	KeyG:            'g',
+	KeyH:            'h',
+	KeyI:            'i',
+	KeyJ:            'j',
+	KeyK:            'k',
+	KeyL:            'l',
+	KeyM:            'm',
+	KeyN:            'n',
+	KeyO:            'o',
+	KeyP:            'p',
+	KeyQ:            'q',
+	KeyR:            'r',
+	KeyS:            's',
+	KeyT:            't',
+	KeyU:            'u',
+	KeyV:            'v',
+	KeyW:            'w',
+	KeyX:            'x',
+	KeyY:            'y',
+	KeyZ:            'z',
+	Key1:            '1',
+	Key2:            '2',
+	Key3:            '3',
+	Key4:            '4',
+	Key5:            '5',
+	Key6:            '6',
+	Key7:            '7',
+	Key8:            '8',
+	Key9:            '9',
+	Key0:            '0',
+	KeyEnter:        '\r',
+	KeyEsc:          '\x1b',
+	KeyBackspace:    '\x7f',
+	KeyTab:          '\t',
+	KeySpace:        ' ',
+	KeyMinus:        '-',
+	KeyEqual:        '=',
+	KeyLBrace:       '[',
+	KeyRBrace:       ']',
+	KeyBackslash:    '\\',
+	KeyIsoHash:      '#',
+	KeySemi:         ';',
+	KeyQuote:        '\'',
+	KeyGrave:        '`',
+	KeyComma:        ',',
+	KeyPeriod:       '.',
+	KeySlash:        '/',
+	KeyCapsLock:     57357,
+	KeyPrtScr:       57361,
+	KeyScrLock:      57359,
+	KeyPause:        57362,
+	KeyNumLock:      57360,
+	KeyPadDiv:       57410,
+	KeyPadMul:       57411,
+	KeyPadSub:       57412,
+	KeyPadAdd:       57413,
+	KeyPadEnter:     57414,
+	KeyPad1:         57400,
+	KeyPad2:         57401,
+	KeyPad3:         57402,
+	KeyPad4:         57403,
+	KeyPad5:         57404,
+	KeyPad6:         57405,
+	KeyPad7:         57406,
+	KeyPad8:         57407,
+	KeyPad9:         57408,
+	KeyPad0:         57399,
+	KeyPadDec:       57409,
+	KeyIsoBackSlash: '\\', // TODO: does kitty have another mapping for this?
+	KeyMenu:         57363,
+	KeyPadEqual:     57415,
+	KeyF13:          57376,
+	KeyF14:          57377,
+	KeyF15:          57378,
+	KeyF16:          57379,
+	KeyF17:          57380,
+	KeyF18:          57381,
+	KeyF19:          57382,
+	KeyF20:          57383,
+	KeyF21:          57384,
+	KeyF22:          57385,
+	KeyF23:          57386,
+	KeyF24:          57387, // NB: F25 up through 35 are notionally supported by Kitty, but not by us
+	KeyPadComma:     57416,
+	KeyIsoSlash:     '/', // Kitty cannot discriminate?
+	KeyYen:          '¥',
+	KeyLCtrl:        57442,
+	KeyLShift:       57441,
+	KeyLAlt:         57443,
+	KeyLMeta:        57444,
+	KeyRCtrl:        57448,
+	KeyRShift:       57447,
+	KeyRAlt:         57449,
+	KeyRMeta:        57450,
 
-	}
+	// KeyHiragana:   0, // TBD
+	// KeyConvert:    0, // TBD
+	// KeyNonConvert: 0, // TD
 
-	if vk, ok := keys[k]; ok {
-		return vk
-	}
-	// TODO
-	return 0
+	// Windows uses a bunch of HID usages from
+	// the consumer page (0x0c) for media playback, and
+	// other applications. We just ignore them.
 }
 
 // KittyBase returns the corresponding Kitty "base" key for the given USB cod.
@@ -639,116 +611,6 @@ func (k Key) VKey() WinVK {
 // some keys (such as F1) are valid, and recognized by Kitty, but do not use the
 // base key encoding because they use another reporting format.
 func (k Key) KittyBase() BaseKey {
-	var baseKeys = map[Key]BaseKey{
-		KeyA:            'a',
-		KeyB:            'b',
-		KeyC:            'c',
-		KeyD:            'd',
-		KeyE:            'e',
-		KeyF:            'f',
-		KeyG:            'g',
-		KeyH:            'h',
-		KeyI:            'i',
-		KeyJ:            'j',
-		KeyK:            'k',
-		KeyL:            'l',
-		KeyM:            'm',
-		KeyN:            'n',
-		KeyO:            'o',
-		KeyP:            'p',
-		KeyQ:            'q',
-		KeyR:            'r',
-		KeyS:            's',
-		KeyT:            't',
-		KeyU:            'u',
-		KeyV:            'v',
-		KeyW:            'w',
-		KeyX:            'x',
-		KeyY:            'y',
-		KeyZ:            'z',
-		Key1:            '1',
-		Key2:            '2',
-		Key3:            '3',
-		Key4:            '4',
-		Key5:            '5',
-		Key6:            '6',
-		Key7:            '7',
-		Key8:            '8',
-		Key9:            '9',
-		Key0:            '0',
-		KeyEnter:        '\r',
-		KeyEsc:          '\x1b',
-		KeyBackspace:    '\x7f',
-		KeyTab:          '\t',
-		KeySpace:        ' ',
-		KeyMinus:        '-',
-		KeyEqual:        '=',
-		KeyLBrace:       '[',
-		KeyRBrace:       ']',
-		KeyBackslash:    '\\',
-		KeyIsoHash:      '#',
-		KeySemi:         ';',
-		KeyQuote:        '\'',
-		KeyGrave:        '`',
-		KeyComma:        ',',
-		KeyPeriod:       '.',
-		KeySlash:        '/',
-		KeyCapsLock:     57357,
-		KeyPrtScr:       57361,
-		KeyScrLock:      57359,
-		KeyPause:        57362,
-		KeyNumLock:      57360,
-		KeyPadDiv:       57410,
-		KeyPadMul:       57411,
-		KeyPadSub:       57412,
-		KeyPadAdd:       57413,
-		KeyPadEnter:     57414,
-		KeyPad1:         57400,
-		KeyPad2:         57401,
-		KeyPad3:         57402,
-		KeyPad4:         57403,
-		KeyPad5:         57404,
-		KeyPad6:         57405,
-		KeyPad7:         57406,
-		KeyPad8:         57407,
-		KeyPad9:         57408,
-		KeyPad0:         57399,
-		KeyPadDec:       57409,
-		KeyIsoBackSlash: '\\', // TODO: does kitty have another mapping for this?
-		KeyMenu:         57363,
-		KeyPadEqual:     57415,
-		KeyF13:          57376,
-		KeyF14:          57377,
-		KeyF15:          57378,
-		KeyF16:          57379,
-		KeyF17:          57380,
-		KeyF18:          57381,
-		KeyF19:          57382,
-		KeyF20:          57383,
-		KeyF21:          57384,
-		KeyF22:          57385,
-		KeyF23:          57386,
-		KeyF24:          57387, // NB: F25 up through 35 are notionally supported by Kitty, but not by us
-		KeyPadComma:     57416,
-		KeyIsoSlash:     '/', // Kitty cannot discriminate?
-		KeyYen:          '¥',
-		KeyLCtrl:        57442,
-		KeyLShift:       57441,
-		KeyLAlt:         57443,
-		KeyLMeta:        57444,
-		KeyRCtrl:        57448,
-		KeyRShift:       57447,
-		KeyRAlt:         57449,
-		KeyRMeta:        57450,
-
-		// KeyHiragana:   0, // TBD
-		// KeyConvert:    0, // TBD
-		// KeyNonConvert: 0, // TD
-
-		// Windows uses a bunch of HID usages from
-		// the consumer page (0x0c) for media playback, and
-		// other applications. We just ignore them.
-	}
 	if w, ok := baseKeys[k]; ok {
 		return w
 	}
